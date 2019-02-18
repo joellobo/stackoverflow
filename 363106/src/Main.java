1e3f9edf452c2826f2d7f0aeffd592171da66b73
64943127304 <joellobo@gmail.com>
2019-02-18 12:33:33 -0300

    other resolution

--- 363106/src/Main.java
+++ 363106/src/Main.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+	/*
 	public static void main(String[] args) throws IOException {
 		char controlador = 'S';
 		do { 
@@ -13,4 +14,15 @@ public class Main {
 	        controlador = resposta.charAt(0); 
 		} while(controlador != 'N');
 	}
+	*/
+	
+	public static void main(String[] args) throws IOException {
+		String resposta = null;
+		do { 
+			System.out.println("Digite: S para continuar e N para parar");
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            resposta = br.readLine();
+		} while(resposta != null && resposta.charAt(0) != 'N');
+	}
+
 }
